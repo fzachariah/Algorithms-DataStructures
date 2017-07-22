ab030cafa267d14bad9ede62ddb2f2a8df1506af
FEBIN ZACHARIAH <febinzachariah6@gmail.com>
2017-07-22 00:36:06 -0400

    Bit Manipulation-Easy

---
+++ BitManipulation/PowerFour.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean isPowerOfFour(int num) {
+        
+        if(num>0 && (num & (num-1))==0 && ((num & 0x55555555)!=0))
+        {
+            return true;
+        }
+        
+        
+        return false;
+        
+        
+        
+    }
+}
